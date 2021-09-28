ee9749b5bf2169f589c9ba017dcd883bcbf19972
unknown <Parkseyoun>
2021-09-28 17:08:27 +0900

    CBV로 블로그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [#서버IP/blog/
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+urlpatterns = [# 서버IP/blog/
+#    path('<int:pk>/', views.single_post_page),
+#    path('', views.index),
+
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
 ]
