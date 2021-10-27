0dce783380c061f9199ebd36911ae815ff132b9b
unknown <Parkseyoun>
2021-10-27 15:33:07 +0900

    포스트 목록/상세 페이지에 카테고리 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -2,45 +2,47 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
-# 모델 = 데이터를 저장하는 단위, 웹페이지에서 add-post 클릭하면 나오는 기능들 모음
+class Category(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+    slug = models.SlugField(max_length=200,unique=True,allow_unicode=True)
 
-# 모델 필드를 추가할때마다 migration 해줘야한다.
-# (venv) python manage.py makemigrations
-# (venv) python manage.py migrate
-# blank=True 필수는 아니다, 데이터가 없어도 생성 >> if~ else 구분안해도 문제는 없다. if~ else 필수는 head_image 처럼 case 가 다를때
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = 'Categories'
 
-class Post(models.Model): # Post라는 모델 클래스, 모델 클래스 이름 = Post
-    title = models.CharField(max_length=30) # title 모델 필드, 글자수 제한
-    hook_text = models.CharField(max_length=100, blank = True) # 블로그 페이지가 돋보이게
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank = True)
+    content = models.TextField()
 
-    content = models.TextField() # content 모델 필드, 무한대 입력
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank =True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank =True)
 
-    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank =True) # 이미지 업로드한 날짜를 기준으로 설정
-    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank =True) # 파일 업로드한 날짜를 기준으로 설정
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    created_at = models.DateTimeField(auto_now_add=True) # Date 모델 필드, Time 모델 필드 자동으로 날짜 생성해 추가
-    # 새로 작성했을 때 생성 : auto_now_add
-    updated_at = models.DateTimeField(auto_now=True) #Model을 생성때마다 migration 해줘
-    # 수정했을 때 업데이트 : auto_now
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-
-    # self = 자기 자신을 의미함
-
-    def __str__(self): # _str_ 함수로 모델의 string 표현 방법 정의하기, 그냥 {{p}}일 때 출력되는 pk와 title
-        return f'[{self.pk}]{self.title} :: {self.author}' # 번호[Primary Key]와 타이틀을 합해 인덱스에 표시
+    category = models.ForeignKey(Category, null=True, blank=True , on_delete=models.SET_NULL)
 
 
-    def get_absolute_url(self): ## 목록페이지와 상세페이지를 연결함
-        return f'/blog/{self.pk}' ## get_absolute_rul 함수를 생성하면, admin 사이트에 자동으로 VIEW ON SITE 버튼이 생성, 누르면 상세페이지 이동
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
 
-    ## 1. 파일 업로드 함수, 이름 가지고옴
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}'
+
+
+
 
     def get_file_name(self):
-        return os.path.basename(self.file_upload.name) #os.path에 있는 basename함수 가지고 오기 자기자신의 파일 업로드된 이름을 가지고옴
+        return os.path.basename(self.file_upload.name)
 
 
     ## 2. 파일 업로드 함수, 확장자 가지고옴
--- blog/tests.py
+++ blog/tests.py
@@ -1,16 +1,37 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Category
 
 
 class TestView(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user_james = User.objects.create_user(username='James', passwd='somepassword')
-        self.user_trump = User.objects.create_user(username='Trump', passwd='somepassword')
+        self.user_james = User.objects.create_user(username='James', password='somepassword')
+        self.user_trump = User.objects.create_user(username='Trump', password='somepassword')
 
+        self.category_programming = Category.object.create(name = 'programming',slug='programming')
+        self.category_culture = Category.object.create(name='culture', slug='culture')
+
+        # 포스트(게시물)이 3개 존재하는 경우
+        self.post_001 = Post.objects.create(
+            title = '첫 번째 포스트입니다.',
+            content = 'Hello World!! We are the world...',
+            author = self.user_james,
+            category = self.category_programming
+        )
+        self.post_002 = Post.objects.create(
+            title = '두 번째 포스트입니다.',
+            content = '1등이 전부가 아니잖아요',
+            author = self.user_trump,
+            category = self.category_culture
+        )
+        self.post_003 = Post.objects.create(
+            title = '세 번째 포스트입니다.',
+            content = '세 번째 포스트입니다.',
+            author = self.user_trump
+        )
     def navbar_test(self, soup):
         # 네비게이션바가 있다
         navbar = soup.nav
@@ -31,63 +52,64 @@ class TestView(TestCase):
         about = navbar.find('a', text='About Me')
         self.assertEqual(about.atrrs['href'], '/about_me/')
 
+    def category_test(self,soup):
+        category = soup.find('div',id = 'categories-card')
+        self.assertIn('Categories',category.text)
+        self.assertIn(f'{self.category_programming.name} ({self.category_programming.post_set.count()})', category.text)
+        self.assertIn(f'{self.category_culture.name} ({self.category_culture.post_set.count()})', category.text)
+        self.assertIn(f'미분류 (1)', category.text)
+
     def test_post_list(self):
-        # 포스트 목록 페이지를 가져온다 // response 변수에 self.client.get('url') 특정 url을 넣어 페이지를 가지고 온다.
+        self.assertEqual(Post.objects.count(),3)
+
+        # 포스트 목록 페이지를 가져온다
         response = self.client.get('/blog/')
-
-        # 정상적으로 페이지가 로드 // status_code 활용해 정상적으로 로드되었는지 확인한다. 200 = OK, 400 = Bad Request, 404 Not Found = 요청한 페이지가 없다
+        # 정상적으로 페이지가 로드
         self.assertEqual(response.status_code, 200)  # 상태가 200과 같은가?
-
-        # 페이지 타이틀 'Blog' // html 을 분석한다 = parser, 분석된 결과를 soup 변수로 받겠다.
+        # 페이지 타이틀 'Blog'
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')  # soup 이 받은 분석된 내용의 제목이 'Blog'와 같은가?
 
         self.navbar_test(soup)
+        self.category_test(soup)
 
-        # 포스트(게시물)이 하나도 없는 경우에
-        self.assertEqual(Post.objects.count(), 0)  # Post 모델 import
+        # 포스트(게시물)의 타이틀이 3개 존재하는가
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('아직 게시물이 없습니다.', main_area.text)
 
-        # 적절한 안내 문구가 포함되어 있는지 // id = 'main-area'를 출력하는 부분에 추가한다.
-        main_area = soup.find('div', id='main-area')  # 앞에서 받은 soup html 분석 결과에서 <div> 태그에 id가 'main-area'인것을 find 찾는다
-        self.assertIn('아직 게시물이 없습니다.', main_area.text)  # 찾은 <div>태그의 text에 '아직~'문장이 포함되는가?
-
-        post_001 = Post.objects.create(
-            title = '첫 번째 포스트입니다.',
-            content = 'Hello World!! We are the world...',
-            author = self.user_james
-        )
-        post_002 = Post.objects.create(
-            title = '두 번째 포스트입니다.',
-            content = '1등이 전부가 아니잖아요',
-            author = self.user_trump
-        )
-        self.assertEqual(Post.objects.count(),2) # post의 갯수가 2개인가?
-
-        # 목록페이지를 새롭게 불러와서 //새로 생성했기 때문에 목록페이지를 다시 불러온다.
-        response = self.client.get('/blog/')
-        self.assertEqual(response.status_code,200)
-        soup = BeautifulSoup(response.content, 'html.parser') # 위에 코드 복붙
-
-        # 포스트(게시물)의 타이틀이 2개 존재하는가
         main_area = soup.find('div', id='main-area') # 앞에서 받은 soup html 분석 결과에서 <div> 태그에 id가 'main-area'인것을 find 찾는다
-        self.assertIn(post_001.title, main_area.text) # main_area의 텍스트에서 post_001의 제목이 포함되는가?
-        self.assertIn(post_002.title, main_area.text) # main_area의 텍스트에서 post_002의 제목이 포함되는가?
+
+        post_001_card = main_area.find('div', id='post-1')
+        self.assertIn(self.post_001.title, post_001_card.text) # main_area의 텍스트에서 post_001의 제목이 포함되는가?
+        self.assertIn(self.post_001.category.name, post_001_card.text)
+
+        post_002_card = main_area.find('div', id='post-2')
+        self.assertIn(self.post_002.title, post_002_card.text) # main_area의 텍스트에서 post_001의 제목이 포함되는가?
+        self.assertIn(self.post_002.category.name, post_002_card.text)
+
+        post_003_card = main_area.find('div', id='post-3')
+        self.assertIn(self.post_003.title, post_003_card.text) # main_area의 텍스트에서 post_001의 제목이 포함되는가?
+        self.assertIn('미분류', post_003_card.text)
 
         self.assertNotIn('아직 게시물이 없습니다.',main_area.text) # main_area의 텍스트에 '아직~'문장이 포함되는가?
         self.assertIn(self.user_james.username.upper(),main_area.text)
         self.assertIn(self.user_trump.username.upper(), main_area.text)
 
-    def test_post_detail(self): # 디테일 상세페이지에 대한 함수
-        # 포스트 하나 생성
-        post_001 = Post.objects.create(
-            title = '첫 번째 포스트입니다.',
-            content = 'Hello World!! We are the world...',
-            author = self.user_james
+        # 포스트(게시물)이 하나도 없는 경우에
+        Post.objects.all().delete
+        self.assertEqual(Post.objects.count(), 0)  # Post 모델 import
+        # 포스트 목록 페이지를 가져온다
+        response = self.client.get('/blog/')
+        # 정상적으로 페이지가 로드
+        self.assertEqual(response.status_code, 200)  # 상태가 200과 같은가?
+        # 페이지 타이틀 'Blog'
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-        )
+
+    def test_post_detail(self): # 디테일 상세페이지에 대한 함수
 
         # 이 포스트의 url이 /blog/1
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1') # 상세페이지에 대한 주소
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1') # 상세페이지에 대한 주소
 
         # url에 의해 정상적으로 상세페이지를 불러오는가
         response = self.client.get('/blog/1/')
@@ -95,19 +117,19 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser') #위에 코드 복붙
 
         self.navbar_test(soup)
+        self.category_test(soup)
 
         # 포스트의 title은 웹브라우저의 title에 있는가
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
 
         # 포스트의 title은 포스트영역에도 있는가
         main_area = soup.find('div', id='main-area') # 위에 코드 복붙
         post_area = main_area.find('div',id = "post-area") # 포스트 영역 추가
-        self.assertIn(post_001.title, post_area.text) # 위에 코드 복붙
-
+        self.assertIn(self.post_001.title, post_area.text) # 위에 코드 복붙
+        self.assertIn(self.post_001.category.name, post_area.text) # 위에 코드 복붙
         # 포스트 작성자가 있는가
         # 아직 작성중
-
         # 포스트의 내용이 있는가
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
 
         self.assertIn(self.user_james.username.upper(),post_area.text)
