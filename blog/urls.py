5c21f2c8ed5327ec608223a5c91bf75f0764a225
unknown <Parkseyoun>
2021-11-03 13:29:36 +0900

    포스트 목록/상세페이지에 태그 기능 추가 및 태그페이지

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('category/<str:slug>', views.category_page),
+urlpatterns = [ #서버IP/blog/
+
+    path('tag/<str:slug>', views.tag_page),
+    path('category/<str:slug>', views.category_page),#서버IP/blog/category/slug
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class PostList(ListView) :
@@ -40,4 +40,16 @@ def category_page(request,slug):
 
                 }
                 )
+def tag_page(request,slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all() #Post.objects.filter(tags=tag)
 
+    return render(request, 'blog/post_list.html',
+                {
+                    'post_list' : post_list,
+                    'categories' : Category.objects.all(),
+                    'no_category_post_count' : Post.objects.filter(category=None).count(),
+                    'tag' : tag
+
+                }
+                )
