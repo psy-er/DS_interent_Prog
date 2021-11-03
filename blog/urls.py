325dee3d847c7f3b911f7d13a8af39dd23f396d4
unknown <Parkseyoun>
2021-11-03 14:04:42 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [ #서버IP/blog/
 
+    path('create_post/',views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page),#서버IP/blog/category/slug
     path('<int:pk>/', views.PostDetail.as_view()),
