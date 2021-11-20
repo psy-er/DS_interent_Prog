9f9d09ab3baf8f3885fa05b1553bb5365d0dc759
unknown <Parkseyoun>
2021-11-21 00:14:17 +0900

    검색 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ #서버IP/blog/
+
+
+    path('search/<str:q>/', views.PostSearch.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/',views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
